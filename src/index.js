234255f1d0de700a7a88d521243e0c244abd3ac4
Alfredo <alfredo.gonzalez.338@my.csun.edu>
2024-05-31 18:59:44 -0700

    pls

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./static/css/main.e73068ed.css";
-import "/firstsearch/build/static/css"
+import "/firstsearch/build/static/css";
 
 import App from "./App";
 
